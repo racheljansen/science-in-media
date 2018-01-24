fcbe3ae99608ce5c91e0b3177269cf97fd605123
Alexandra Paxton <paxton.alexandra@gmail.com>
2018-01-24 15:49:05 -0800

    Add most recent copy of Bartlett to repo

---
+++ bartlett/static/scripts/experiment.js
@@ -0,0 +1,92 @@
+var my_node_id;
+
+// Consent to the experiment.
+$(document).ready(function() {
+
+  // do not allow user to close or reload
+  dallinger.preventExit = true;
+
+  // Print the consent form.
+  $("#print-consent").click(function() {
+    window.print();
+  });
+
+  // Consent to the experiment.
+  $("#consent").click(function() {
+    store.set("hit_id", dallinger.getUrlParameter("hit_id"));
+    store.set("worker_id", dallinger.getUrlParameter("worker_id"));
+    store.set("assignment_id", dallinger.getUrlParameter("assignment_id"));
+    store.set("mode", dallinger.getUrlParameter("mode"));
+
+    dallinger.allowExit();
+    window.location.href = '/instructions';
+  });
+
+  // Consent to the experiment.
+  $("#no-consent").click(function() {
+    dallinger.allowExit();
+    window.close();
+  });
+
+  // Consent to the experiment.
+  $("#go-to-experiment").click(function() {
+    dallinger.allowExit();
+    window.location.href = '/exp';
+  });
+
+  $("#finish-reading").click(function() {
+    $("#stimulus").hide();
+    $("#response-form").show();
+    $("#submit-response").removeClass('disabled');
+    $("#submit-response").html('Submit');
+  });
+
+  $("#submit-response").click(function() {
+    $("#submit-response").addClass('disabled');
+    $("#submit-response").html('Sending...');
+
+    var response = $("#reproduction").val();
+
+    $("#reproduction").val("");
+
+    dallinger.createInfo(my_node_id, {
+      contents: response,
+      info_type: 'Info'
+    }).done(function (resp) {
+      create_agent();
+    });
+  });
+
+});
+
+// Create the agent.
+var create_agent = function() {
+  $('#finish-reading').prop('disabled', true);
+  dallinger.createAgent()
+    .done(function (resp) {
+      $('#finish-reading').prop('disabled', false);
+      my_node_id = resp.node.id;
+      get_info();
+    })
+    .fail(function () {
+      dallinger.allowExit();
+      dallinger.goToPage('questionnaire');
+    });
+};
+
+var get_info = function() {
+  dallinger.getReceivedInfos(my_node_id)
+    .done(function (resp) {
+      var story = resp.infos[0].contents;
+      var storyHTML = markdown.toHTML(story);
+      $("#story").html(storyHTML);
+      $("#stimulus").show();
+      $("#response-form").hide();
+      $("#finish-reading").show();
+    })
+    .fail(function (err) {
+      console.log(err);
+      var errorResponse = JSON.parse(err.response);
+      $('body').html(errorResponse.html);
+    });
+};
